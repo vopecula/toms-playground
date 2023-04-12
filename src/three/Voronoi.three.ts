564d5aecaa0fe22d0c893a91bb84bd8c0e2abd23
Tamas Kecskes <TKECSKES@novomatic.com>
2023-04-12 09:18:40 +0200

    Fixed typos

--- src/three/Voronoi.three.ts
+++ src/three/Voronoi.three.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { plainText as fragmentShader } from './shaders/voronoi.frag'
